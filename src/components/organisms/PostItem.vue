87477802b019fc24f131dc6840a8590dff997c13
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-06-15 22:08:21 +0900

    個々の記事を指すPostItemコンポーネントを作成し、表示 #7

---
+++ src/components/organisms/PostItem.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { Post } from "@/types/Post";
+
+defineProps<{
+  post: Post;
+}>();
+</script>
+
+<template>
+  <li class="border">
+    <h1 class="font-bold">{{ post.title }}</h1>
+    <nav>
+      <p>by {{ post.user.name }}</p>
+    </nav>
+  </li>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,30 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { axios } from '@/lib/axios';
 import { useAuthUserStore } from '@/stores/authUser';
 import PostFormLink from '../components/atoms/PostFormLink.vue';
+import PostItem from '../components/organisms/PostItem.vue';
 
 const authUser: any = useAuthUserStore();
+
+const posts = ref();
+onMounted(async () => {
+  const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/posts`);
+  posts.value = res.data;
+});
 </script>
 
 <template>
   <h1 class="font-bold">トップページ</h1>
   <p>Welcome! {{ authUser.info ? authUser.info.name : "stranger" }}</p>
+
   <nav class="text-center">
     <PostFormLink />
   </nav>
+
+  <ul>
+    <template v-for="post in posts" :key="post.id">
+      <PostItem :post="post" />
+    </template>
+  </ul>
 </template>
