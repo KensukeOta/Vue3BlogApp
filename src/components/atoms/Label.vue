63fda7c51186c015f761a8db85f455ba5c786d81
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-06-15 15:38:57 +0900

    「必須」タグを付与 #1

--- src/components/atoms/Label.vue
+++ src/components/atoms/Label.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
+import { useRoute } from 'vue-router';
+
 const props = defineProps<{
   for: string;
 }>();
+
+const route = useRoute();
 </script>
 
 <template>
   <label :for="props.for">
-    <slot></slot>
+    <slot></slot> <span class="text-red-500">{{ route.path === "/signup" ? "*必須" : null }}</span>
   </label>
 </template>
