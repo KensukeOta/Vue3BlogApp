0aa1b9a96210c04a7491c3bb57711f9c68215aba
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-06-15 23:26:09 +0900

    個々の記事の内容を表示する処理を実装 #9

--- src/views/PostContentView.vue
+++ src/views/PostContentView.vue
@@ -1,5 +1,20 @@
 <script setup lang="ts">
+import type { Post } from '@/types/Post';
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import { axios } from '@/lib/axios';
+
+const post = ref<Post>();
+
+const route = useRoute();
+
+onMounted(async () => {
+  const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/posts/${route.params.id}`);
+  post.value = res.data;
+});
 </script>
 
 <template>
+  <h1 class="font-bold">{{ post?.title }}</h1>
+  <p>{{ post?.body }}</p>
 </template>
